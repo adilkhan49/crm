bb40b4c154b20b6bbfdc2d5dc77d7eb1d60bdcd7
adil <adilkhan49@hotmail.com>
2023-07-31 15:13:48 +0100

    added login logout functionality

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView # new
 
 urlpatterns = [
     path('humanity/',include("humanity.urls")),
     path('admin/', admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")), 
+    path('', TemplateView.as_view(template_name='registration/home.html'), name='home'), 
+    
+
 ]
